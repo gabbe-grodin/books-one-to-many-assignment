e9ff6dca3e393880599dbf0f8e7cca017007d66f
Gabbe Grodin, MFA <gabbe.grodin@gmail.com>
2023-08-14 22:02:20 -0500

    added create book functionality and styling

--- flask_app/controllers/books.py
+++ flask_app/controllers/books.py
@@ -2,6 +2,17 @@ from flask import render_template, redirect, request, session, flash
 from flask_app import app
 from flask_app.models import book, author
 
+@app.route('/')
+def home():
+    return redirect("/books")
+
 @app.route('/books')
-def get_all():
-    return render_template('index.html')
+def show_all_books():
+    all_books=book.Book.get_all_books()
+    print(all_books[0], "*"*20)
+    return render_template('index.html',book_list=all_books)
+
+@app.route('/add/book', methods=['POST'])
+def add_book():
+    book.Book.add_book_to_db(request.form)
+    return redirect('/books')
---
+++ flask_app/models/book.py
@@ -0,0 +1,38 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import author
+
+
+
+class Book:
+    db='books'
+    def __init__(self,data):
+        self.id=data['id']
+        self.title=data['title']
+        self.pages=data['pages']
+        self.created_at=data['created_at']
+        self.updated_at=data['updated_at']
+        self.faved_by=[]
+
+    @classmethod
+    def add_book_to_db(cls,data):
+        query="""
+              INSERT INTO books(title,pages)
+              VALUES(%(title)s,%(pages)s)
+              """
+        data={"title":data["title"],
+              "pages":data["pages"]}
+        result=connectToMySQL(cls.db).query_db(query,data)
+        return result
+    
+    @classmethod
+    def get_all_books(cls):
+        query="""
+              SELECT * FROM books
+              """
+        result=connectToMySQL(cls.db).query_db(query)
+        print("RESULTS: ", result)
+        books=[]
+        for book in result:
+            books.append(cls(book))
+            print("HERE ARE ALL THE BOOKS:",books)
+        return books
